dd6194054b45aee4602cf8ef5f8c1efb3e639da0
codegrann <omondihills02@gmail.com>
2023-06-28 10:36:07 +0300

    .

--- src/widgets/MyCustomWidget.js
+++ src/widgets/MyCustomWidget.js
@@ -43,50 +43,52 @@ export default function MyCustomWidget() {
         return (kelvin - 273.15) * (9 / 5) + 32;
       };
   
-    return (
-      <div style={{ minWidth: 300 }}>
-        {weatherData && (
-          <div>
-            <h2 style={{ fontSize: '1.2rem', fontWeight: 500 }}>Today's Weather</h2>
-            <p>
-              Temperature: 
-              <span style={{ fontSize: '1.2rem' }}>
-                {convertKelvinToFahrenheit(weatherData.main.temp).toFixed(1)}°F  
-                <i className="bi bi-thermometer-half" style={{ color: 'blue' }}></i>
-              </span>
-            </p>
-            <p>
-              Humidity: 
-              <span style={{ fontSize: '1.2rem' }}>
-                {weatherData.main.humidity}%  
-                <i className="bi bi-droplet" style={{ color: 'blue' }}></i>
-              </span>
-            </p>
-            <p>
-              Weather Conditions: 
-              <span style={{ fontSize: '1.2rem' }}>
-                {weatherData.weather[0].main}  
-                <i className="bi bi-cloud-sun" style={{ color: 'blue' }}></i>
-              </span>
-            </p>
-            <p>
-              Visibility: 
-              <span style={{ fontSize: '1.2rem' }}>
-                {weatherData.visibility} m  
-                <i className="bi bi-eye" style={{ color: 'blue' }}></i>
-              </span>
-            </p>
-            <p>
-              Wind Speed: 
-              <span style={{ fontSize: '1.2rem' }}>
-                {weatherData.wind.speed} m/s  
-                <i className="bi bi-wind" style={{ color: 'blue' }}></i>
-              </span>
-            </p>
-          </div>
-        )}
-      </div>
-    );
-  }
+      return (
+        <div style={{ minWidth: 300, lineHeight: 1.5 }}>
+          {weatherData && (
+            <div>
+              <h2 style={{ fontSize: '1.2rem', fontWeight: 500 }}>Today's Weather</h2>
+              <p>
+                Temperature:{' '} 
+                <span style={{ fontSize: '1.2rem' }}>
+                  {convertKelvinToFahrenheit(weatherData.main.temp).toFixed(1)}°F {' '}  
+                  <i className="bi bi-thermometer-half" style={{ color: 'blue' }}></i>
+                </span>
+              </p>
+              <p>
+                Humidity:{' '}
+                <span style={{ fontSize: '1.2rem' }}>
+                  {weatherData.main.humidity}%  {' '}
+                  <i className="bi bi-droplet" style={{ color: 'blue' }}></i>
+                </span>
+              </p>
+              <p>
+                Weather Conditions: {' '}
+                <span style={{ fontSize: '1.2rem' }}>
+                  {weatherData.weather[0].main} {' '} 
+                  <i className="bi bi-cloud-sun" style={{ color: 'blue' }}></i>
+                </span>
+              </p>
+              <p>
+                Visibility: {' '}
+                <span style={{ fontSize: '1.2rem' }}>
+                  {weatherData.visibility} m  {' '}
+                  <i className="bi bi-eye" style={{ color: 'blue' }}></i>
+                </span>
+              </p>
+              <p>
+                Wind Speed: {' '}
+                <span style={{ fontSize: '1.2rem' }}>
+                  {weatherData.wind.speed} m/s  {' '}
+                  <i className="bi bi-wind" style={{ color: 'blue' }}></i>
+                </span>
+              </p>
+            </div>
+          )}
+           
+        </div>
+      );
+    }
+    
   
-
+  
