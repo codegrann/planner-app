fc582ef77b30c337b6b5580badf54d14788307b5
codegrann <omondihills02@gmail.com>
2023-06-28 10:29:49 +0300

    .added my custom widget

--- src/widgets/MyCustomWidget.js
+++ src/widgets/MyCustomWidget.js
@@ -47,9 +47,42 @@ export default function MyCustomWidget() {
       <div style={{ minWidth: 300 }}>
         {weatherData && (
           <div>
-            <h2>Current Weather</h2>
-            <p>Temperature: {convertKelvinToFahrenheit(weatherData.main.temp).toFixed(1)}°F</p>
-            <p>Humidity: {weatherData.main.humidity}%</p>
+            <h2 style={{ fontSize: '1.2rem', fontWeight: 500 }}>Today's Weather</h2>
+            <p>
+              Temperature: 
+              <span style={{ fontSize: '1.2rem' }}>
+                {convertKelvinToFahrenheit(weatherData.main.temp).toFixed(1)}°F  
+                <i className="bi bi-thermometer-half" style={{ color: 'blue' }}></i>
+              </span>
+            </p>
+            <p>
+              Humidity: 
+              <span style={{ fontSize: '1.2rem' }}>
+                {weatherData.main.humidity}%  
+                <i className="bi bi-droplet" style={{ color: 'blue' }}></i>
+              </span>
+            </p>
+            <p>
+              Weather Conditions: 
+              <span style={{ fontSize: '1.2rem' }}>
+                {weatherData.weather[0].main}  
+                <i className="bi bi-cloud-sun" style={{ color: 'blue' }}></i>
+              </span>
+            </p>
+            <p>
+              Visibility: 
+              <span style={{ fontSize: '1.2rem' }}>
+                {weatherData.visibility} m  
+                <i className="bi bi-eye" style={{ color: 'blue' }}></i>
+              </span>
+            </p>
+            <p>
+              Wind Speed: 
+              <span style={{ fontSize: '1.2rem' }}>
+                {weatherData.wind.speed} m/s  
+                <i className="bi bi-wind" style={{ color: 'blue' }}></i>
+              </span>
+            </p>
           </div>
         )}
       </div>
